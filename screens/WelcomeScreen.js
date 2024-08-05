05c9bb290a9101ef309f69ffe478698ac5bf601f
MohamedMohamed <m01126165016@icloud.com>
2024-08-05 03:59:33 +0300

    Final project assessment

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,18 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
+import { navigate } from './../node_modules/@react-navigation/routers/src/CommonActions';
 
 const WelcomeScreen = ({ navigation }) => {
   // Add welcome screen code here.
-  return <View></View>;
+  return (
+    <View>
+      <Image source={require('/assets/little-lemon-logo.png')} />
+      <Text>Little Lemon, your local Mediterranean Bistro</Text>
+      <Pressable onPress={() => navigation.navigate('Subscribe')}>
+        <Text>Newsletter</Text>
+      </Pressable>
+    </View>
+  )
 };
 
 export default WelcomeScreen;
